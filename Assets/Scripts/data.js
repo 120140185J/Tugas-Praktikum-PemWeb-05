4ab7dd73b8f8ae8c0bcddf0275b8cbdd1fce44dc
120140185J <justin.120140185@student.itera.ac.id>
2022-12-12 22:55:50 +0700

    Push

---
+++ Assets/Scripts/data.js
@@ -0,0 +1,93 @@
+const Mhs = [
+    {
+        nama: 'Justin Halim',
+        nim: 120140185,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Uzumaki Naruto',
+        nim: 120140000,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Monkey D. Luffy',
+        nim: 120140999,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Yoasobi',
+        nim: 120140998,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Aimer',
+        nim: 120140997,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Eve',
+        nim: 120140996,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'LiSA',
+        nim: 120140995,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Zhongli',
+        nim: 120140994,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Kamisato Ayato',
+        nim: 120140993,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Kamisato Ayaka',
+        nim: 120140992,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Naganohara Yoimiya',
+        nim: 120140991,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Kuki Shinobu',
+        nim: 120140990,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Arataki Itto',
+        nim: 120140989,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Raiden Shogun',
+        nim: 120140988,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Lesser Lord Kusanali',
+        nim: 120140987,
+        prodi: 'PWK'
+    },
+    {
+        nama: 'Nahida',
+        nim: 120140986,
+        prodi: 'IF'
+    },
+    {
+        nama: 'Al Haitham',
+        nim: 120140985,
+        prodi: 'SI'
+    },
+    {
+        nama: 'Cyno',
+        nim: 120140984,
+        prodi: 'PWK'
+    }
+];
+export {Mhs};
